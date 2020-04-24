1f29264982e51124ffb5615b1bb37fd112742928
Ian Sweeney <173560+spr12ian@users.noreply.github.com>
2020-04-24 12:12:23 +0100

    Install package list

--- wsl_ubuntu_daily.sh
+++ wsl_ubuntu_daily.sh
@@ -1,8 +1,14 @@
+# We expect this script to run as root
 cd $HOME
 pwd
+
 do-release-upgrade
 apt update
 apt full-upgrade -y
+cat ubuntu_packages.txt | xargs apt install -y
 apt autoremove
+
 cd -
+pwd
+
 echo 'Done!'
