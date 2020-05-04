529da613b364620e4f7a74edc54d825ffe7b0d83
Ian Sweeney <173560+spr12ian@users.noreply.github.com>
2020-05-04 10:24:51 +0100

    Fewer directory changes

--- upgrade_apt_packages.sh
+++ upgrade_apt_packages.sh
@@ -1,14 +1,10 @@
 # We expect this script to run as root
-cd $HOME
-pwd
+requiredPackages="~/gitFolders/required_apt_packages.txt"
 
 do-release-upgrade
 apt update
 apt full-upgrade -y
-cat required_apt_packages.txt | xargs apt install -y
+cat $requiredPackages | xargs apt install -y
 apt autoremove -y
-
-cd -
-pwd
 
 echo 'Done!'
